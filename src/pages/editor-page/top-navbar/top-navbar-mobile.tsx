d4def4cbbed81bdc3dd1b3fbb22fa790180b5041
wweziz0001 <wweziz0001@github.com>
2026-03-27 16:05:41 -0400

    finx navbar height

--- src/pages/editor-page/top-navbar/top-navbar-mobile.tsx
+++ src/pages/editor-page/top-navbar/top-navbar-mobile.tsx
@@ -33,7 +33,7 @@ export const TopNavbarMobile: React.FC<TopNavbarMobileProps> = () => {
     return (
         <nav className="flex flex-col justify-between border-b px-3 md:h-12 md:flex-row md:items-center md:px-4">
             <div className="flex flex-1 flex-col justify-between gap-x-1 md:flex-row md:justify-normal">
-                <div className="flex items-center justify-between pt-[8px] font-primary md:py-[0px]">
+                <div className="flex items-center justify-between pt-[8px] font-primary md:py-0">
                     <div className="flex items-center gap-2">
                         <Button
                             size={'icon'}
--- src/pages/editor-page/top-navbar/top-navbar-mock.tsx
+++ src/pages/editor-page/top-navbar/top-navbar-mock.tsx
@@ -9,7 +9,7 @@ export const TopNavbarMock: React.FC = () => {
     return (
         <nav className="flex h-[105px] flex-col justify-between border-b px-3 md:h-12 md:flex-row md:items-center md:px-4">
             <div className="flex flex-1 flex-col justify-between gap-x-1 md:flex-row md:justify-normal">
-                <div className="flex items-center justify-between pt-[8px] font-primary md:py-[0px]">
+                <div className="flex items-center justify-between pt-[8px] font-primary md:py-0">
                     <a
                         href="https://schemadash.io"
                         className="cursor-pointer"
